a6b8366447e7314de64999e6196711cf57cd2e43
Asur_Raj <rajaasiwal981@gmail.com>
2023-02-06 16:20:39 +0530

    font awesome

--- frontend/src/components/Notes.js
+++ frontend/src/components/Notes.js
@@ -6,7 +6,7 @@ const Notes = () => {
   const context = useContext(NoteContext)
   const {notes, setNotes} = context
   return (
-    <div className='row my-3'>
+    <div className='row my-3 mx-2'>
       <h2>Here are my Notes</h2> 
       {notes.map((note)=>{
           return <NoteItem key={note._id} note={note}/> //key sab ki alag honi chahiye. to key de raha hu
