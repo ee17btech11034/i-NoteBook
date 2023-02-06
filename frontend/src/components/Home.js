f21f05f617ce44e7ae40756719a54e22d3725d0c
Asur_Raj <rajaasiwal981@gmail.com>
2023-02-06 15:24:13 +0530

    NewsItem Component

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -1,16 +1,10 @@
-import React, { useContext } from 'react'
-import NoteContext from '../contexts/notes/NoteContext'
+import React from 'react'
+import Notes from './Notes'
 
 const Home = () => {
-  const context = useContext(NoteContext)
-  const {notes, setNotes} = context
   return (
-    <div className='container'>
-      <h2>Here are my Notes</h2> 
-      {notes.map((note)=>{
-          return note.title
-        })}
-
+    <div>
+      <Notes/>
     </div>
   )
 }
--- frontend/src/contexts/notes/NoteState.js
+++ frontend/src/contexts/notes/NoteState.js
@@ -4,9 +4,27 @@ import { useState } from 'react';
 const NoteState = (props)=>{
     const initialNotes = [
         {
-          "_id": "63dce460ec698dcbf7c5581e",
+          "_id": "63dce460ec698dcbf7c5581e0",
           "user": "63dbd6bf2cf99fd58c8ccc95",
           "title": "My note 1",
+          "description": "Checking this note 1 if added",
+          "tag": "Personal",
+          "date": "2023-02-03T10:39:28.734Z",
+          "__v": 0
+        },
+        {
+          "_id": "63dce460ec698dcbf7c5581e1",
+          "user": "63dbd6bf2cf99fd58c8ccc95",
+          "title": "My note 2",
+          "description": "Checking this note 1 if added",
+          "tag": "Personal",
+          "date": "2023-02-03T10:39:28.734Z",
+          "__v": 0
+        },
+        {
+          "_id": "63dce460ec698dcbf7c5581e2",
+          "user": "63dbd6bf2cf99fd58c8ccc95",
+          "title": "My note 3",
           "description": "Checking this note 1 if added",
           "tag": "Personal",
           "date": "2023-02-03T10:39:28.734Z",
